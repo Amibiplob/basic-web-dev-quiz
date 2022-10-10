3b8bcf402c3bad769afad6094300af6a99cf8f84
amibiplob <muhammadbiplob7@gmail.com>
2022-10-10 22:32:46 +0600

    card added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
-import Navbar from './Components/Navbar';
-import Home from './Components/Home';
-
+import Root from './Root'
+import Home from './Components/Home'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Root></Root>,
+    children :[
+      {
+        path : '/',
+        loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+        element :<Home></Home>
+      },
+     {
+       path: 'home',
+       element: <Home></Home>
+     }
+    ]
+  }
+])
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
-      <Home></Home>
+    <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,65 +1,24 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const quiz = useLoaderData([])
+    const quizName = quiz.data ;
+   // console.log(quiz.data)
     return (
-        <div className='flex'>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">
-                        Shoes!
-                        <div className="badge badge-secondary">NEW</div>
-                    </h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">
-                        Shoes!
-                        <div className="badge badge-secondary">NEW</div>
-                    </h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">
-                        Shoes!
-                        <div className="badge badge-secondary">NEW</div>
-                    </h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">
-                        Shoes!
-                        <div className="badge badge-secondary">NEW</div>
-                    </h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
-                    </div>
-                </div>
-            </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-4/5 mx-auto gap-7'>
+           {
+            quizName.map(data =>(
+                <div key={data.id} className="card w-96 bg-base-100 shadow-xl">
+                    <figure className='bg-slate-200 hover:bg-slate-400'><img src={data.logo}/></figure>
+                    <div className="flex justify-between items-center w-11/12 mx-auto">
+                        <div className="badge badge-outline p-3">{data.name}</div>
+                        <button className="btn btn-outline btn-info btn-sm m-2">Responsive</button>
 
+                    </div>
+                </div>
+            ))
+           }
         </div>
     );
 };
